ec8246a31a3da3ee82ae47bce9c660173fd5b3a2
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-08-07 22:23:28 +0300

    Created Details Component Html

--- client/book-reviews/src/app/app-routing.module.ts
+++ client/book-reviews/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './auth-user/register/register.component';
 import { AboutComponent } from './about/about.component';
 import { CatalogComponent } from './books/catalog/catalog.component';
 import { CreateComponent } from './books/create/create.component';
+import { DetailsComponent } from './books/details/details.component';
 
 const routes: Routes = [
   {
@@ -16,23 +17,27 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-  }, 
+  },
   {
     path: 'about',
     component: AboutComponent,
-  }, 
+  },
   {
     path: 'catalog',
     component: CatalogComponent,
-  }, 
+  },
   {
     path: 'create',
     component: CreateComponent,
   },
   {
+    path: 'details',
+    component: DetailsComponent,
+  },
+  {
     path: 'login',
     component: LoginComponent,
-  }, 
+  },
   {
     path: 'register',
     component: RegisterComponent,

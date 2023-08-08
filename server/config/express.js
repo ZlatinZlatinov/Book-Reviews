e7ba6dbe1a5ef4e1c16b41f5eaed26e3a876c5c4
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-08-08 17:22:55 +0300

    added middlewares

--- server/config/express.js
+++ server/config/express.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('../middlewares/cors');
+const session = require('../middlewares/session');
 
 module.exports = (app) => {
     app.use(express.json());
     app.use(cors());
-    // to do: add session
+    app.use(session());
 }
---
+++ server/middlewares/session.js
@@ -0,0 +1,17 @@
+const { pareseToken } = require('../services/userService');
+
+module.exports = () => (req, res, next) => {
+    const token = req.headers['x-authorization'];
+
+    if (token) {
+        try {
+            const payload = pareseToken(token);
+            req.user = payload;
+            req.token = token;
+        } catch (err) {
+            return res.status(401).json({ message: 'Invalid token!' });
+        }
+    }
+
+    next();
+}
